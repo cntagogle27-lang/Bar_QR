5f444fae01060266d6a8a3cba24bc551f9f44835
Nacho Gilgado Millán <cntagogle27@gmail.com>
2026-06-04 17:22:04 +0200

    PrintAgent: soporte Windows Service, instalar como servicio automatico

--- PrintAgent/Program.cs
+++ PrintAgent/Program.cs
@@ -2,6 +2,9 @@ using PrintAgent;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+// Soporte para ejecutar como servicio Windows
+builder.Services.AddWindowsService(o => o.ServiceName = "PrintAgentBarQR");
+
 // Lee la URL del backend desde appsettings.json o variables de entorno
 builder.Services.AddHttpClient("cloud", c =>
 {
